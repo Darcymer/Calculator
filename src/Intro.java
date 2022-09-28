cfe70d2f2f789ad4847eb4ef6b7b597aaf594c46
Darcymer <91292053+Darcymer@users.noreply.github.com>
2022-09-28 12:40:09 -0600

    my calculator code so far

---
+++ src/Intro.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class introduction{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Welcome to the Auto-Calculator!!!");
+        System.out.println();
+        System.out.print("Please write down your name : ");
+
+        String fullname = input.next();
+        System.out.println();
+        System.out.print("So, " +fullname+" here is how this code works, you write down any math question you have");
+        System.out.print( "particularly basic opperators, and we can solve it for you.");
+        System.out.println();
+        System.out.print("Please write down your question:");
+
+
+    }
+
+
+}
