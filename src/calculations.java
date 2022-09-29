c41c1c6c1aef157c0534482f1030d79fa7fee207
Darcymer <91292053+Darcymer@users.noreply.github.com>
2022-09-28 23:44:49 -0600

    figured out classes and files and call by main

--- src/calculations.java
+++ src/calculations.java
@@ -1,32 +1,46 @@
-public class calculations {
-    
-    
-    
-    // Add
 
+class math{
+  // Add
+    float adder (float x, float y){
+        float num;
+        num = x + y;
+        return num;
+    }
 
 
     // Subtract
-
-
-
-
-
+    float subber (float x, float y){
+        float num;
+        num = x - y;
+        return num;
+    }
 
     //Multiply
-
+    float multier (float x, float y){
+        float num;
+        num = x * y;
+        return num;
+    }
 
 
 
 
     //Divide
+    float diviser (float x, float y){
+        float num;
+        num = x / y;
+        return num;
+    }
 
-
-
+    float moder (float x, float y){
+        float num;
+        num = x % y;
+        return num;
+    }
 
     //Show the results
 
-
-
-
 }
+
+
+
--- src/menu.java
+++ src/menu.java
@@ -1,14 +1,15 @@
 import java.util.Scanner; 
 
 public class menu {
-     public static void showmenu(String[]args)
+     public void showmenu()
      {
-     Scanner input = new Scanner(System.in);
-     System.out.println("Enter your choice from the following menu:");
-     System.out.println("1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Exit 6.Show Steps so far 7.Your total");
-     String userinput = input.next();
-    // I should call the Intro class (Ask for name and Instructions)
-    // Than call the menu ( )
-    // Call the Opperators based on what input the user ask
+     System.out.println();
+     System.out.println("1.Addition");
+     System.out.println("2.Subtraction");
+     System.out.println("3.Multiplication");
+     System.out.println("4.Division");
+     System.out.println("5.Your total");
+     System.out.println("0.Exit");
+     System.out.println();
      }
 }
